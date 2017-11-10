bf7d51f48384c7fa73030e4e91913e847e795cd1
Bar Ziony <bartzy@gmail.com>
2017-11-10 18:55:21 +0200

    Update to new Cordova API

--- src/ios/MediaScan.m
+++ src/ios/MediaScan.m
@@ -8,29 +8,26 @@
 @implementation MediaScan
 @synthesize callbackId;
 
-- (void)file:(CDVInvokedUrlCommand*)command
-{
+- (void)file:(CDVInvokedUrlCommand*)command {
     self.callbackId = command.callbackId;
-      
+
     NSURL *imageURL = [NSURL URLWithString:[command.arguments objectAtIndex:0]];
     NSData *imageData = [NSData dataWithContentsOfURL:imageURL];
     UIImage *image = [UIImage imageWithData:imageData];
-    
+
     UIImageWriteToSavedPhotosAlbum(image, self, @selector(image:didFinishSavingWithError:contextInfo:), nil);
 }
 
-- (void)image:(UIImage *)image didFinishSavingWithError:(NSError *)error contextInfo:(void *)contextInfo
-{
-    if (error != NULL) 
-    {
-        CDVPluginResult* result = [CDVPluginResult resultWithStatus: CDVCommandStatus_ERROR messageAsString:error.description];
-        [self.webView stringByEvaluatingJavaScriptFromString:[result toErrorCallbackString: self.callbackId]];
+- (void)image:(UIImage *)image didFinishSavingWithError:(NSError *)error contextInfo:(void *)contextInfo {
+    CDVPluginResult *pluginResult;
+
+    if (error != NULL) {
+        pluginResult = [CDVPluginResult resultWithStatus: CDVCommandStatus_ERROR messageAsString:error.description];
+    } else {
+        pluginResult = [CDVPluginResult resultWithStatus: CDVCommandStatus_OK messageAsString:@"Image saved"];
     }
-    else
-    {
-        CDVPluginResult* result = [CDVPluginResult resultWithStatus: CDVCommandStatus_OK messageAsString:@"Image saved"];
-        [self.webView stringByEvaluatingJavaScriptFromString:[result toSuccessCallbackString: self.callbackId]];
-    }
+
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:self.callbackId];
 }
 
 @end
